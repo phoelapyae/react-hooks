06ba8873f9e0dfc9853fe9e5b50dea8e626d6400
zinmyomaung <zinmyomaung.zotefamily@gmail.com>
2022-02-02 23:11:19 +0630

    useState hook

---
+++ src/useForm.js
@@ -0,0 +1,17 @@
+import React, {useState} from 'react';
+
+const useForm = (initialValues) => {
+  const [values, setValues] = useState(initialValues);
+
+  return [
+    values,
+    e => {
+      setValues({
+        ...values,
+        [e.target.name]: e.target.value  
+      }) 
+    }
+  ]
+}
+
+export default useForm;
